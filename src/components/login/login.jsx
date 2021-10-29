40632e6290a741539cdcf5e298d007b7af44b47c
jisoo <devjisoolee@gmail.com>
2021-10-29 16:16:44 +0900

    Make logout button

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
+  const [isLogin, setIsLogin] = useState(false);
+
   const onLogin = event => {
     authService
       .login(event.currentTarget.textContent) //
-      .then(console.log);
+      .then(setIsLogin(true))
+      .then(routeChange);
   };
+
+  const routeChange = () => {};
+
+  let onLogout = false;
+  if (isLogin === true) {
+    onLogout = () => {
+      authService
+        .logout() //
+        .then(setIsLogin(false))
+        .then((onLogout = false));
+    };
+  }
+
   return (
     <section className={styles.login}>
-      <Header />
+      <Header onLogout={onLogout} />
       <section>
         <h1>Login</h1>
         <ul className={styles.list}>
