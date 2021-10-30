6b7d07d9aed5a802283df37c46f2c821bf9a33b7
jisoo <devjisoolee@gmail.com>
2021-10-30 16:40:13 +0900

    Add editor and preview layout

---
+++ src/components/editor/editor.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './editor.module.css';
+
+const Editor = props => {
+  return (
+    <section className={styles.editor}>
+      <h1 className={styles.title}>Card Maker</h1>
+      dsdasa
+    </section>
+  );
+};
+
+export default Editor;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
-import Footer from '../footer/footer';
 import Header from '../header/header';
+import Footer from '../footer/footer';
 import styles from './maker.module.css';
+import Preview from '../preview/preview';
+import Editor from '../editor/editor';
+
 const Maker = ({ authService }) => {
   const history = useHistory();
 
@@ -16,10 +19,20 @@ const Maker = ({ authService }) => {
         history.push('/');
       }
     });
+    //onAuthStateChanged에서 auth의 변경 감지할 때 실행될 콜백함수 전달
+    //useEffect는 마운트될때만 호출되고 컴포넌트 상태가 변경되면 전달해놓은 콜백함수가 실행?
   });
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        {/* <div className={styles.cardMaker}> */}
+        {/* </div> */}
+        {/* <div className={styles.cardPreview}> */}
+        {/* </div> */}
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
